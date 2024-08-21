71e994102e33c17a9ea3ab56a8e7ce5cbeec7f55
Wojciech Kesek <wojciech.kesek@qualitysolutions.pl>
2024-08-21 16:53:15 +0200

    Wywalone okno z wybiorem search engine - lokatory do aktualizaji

--- src/main/java/tests/DefaultTest.java
+++ src/main/java/tests/DefaultTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
@@ -15,7 +16,9 @@ public abstract class DefaultTest {
     @BeforeTest
     public void setUpTest () {
         System.setProperty( "webdriver.chrome.driver", "D:\\Projekty\\Drivers\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait( 50, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
